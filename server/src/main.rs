ad3d4354a154ec1a431e4c57fc61af1fe820c38d
ahsouza <annibalhsouza@gmail.com>
2022-02-23 14:48:29 -0300

    feat: instanciar canal e adicionar política para permissão na conexão entre clientes e servidor

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,4 +12,18 @@ fn main() {
     // atribuir conexão tcp com host especifícado
     let server = TcpListener::bind(host).expect("falha ao tentar vincular com host");
     server.set_nonblocking(true).expect("falha ao tentar iniciar o non-blocking")
+
+    // elaborando forma de permitir mais de dois clientes se conectar ao servidor
+    let mut piratas = vec![];
+    // instanciar canal para receber torpedos de strings
+    let (tx, rx) = mpsc::channel::<String>;
+
+    loop {
+        // políticas para permissão na conexão entre clientes e o servidor
+        // por socket de fluxo TCP conectado ao endereço do cliente
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Pirata {} conectado", addr)
+        }
+    }
+
 }
